b48c6d82fed325c140931c034859928ed6c2d3ba
minefreak19 <61090340+minefreak19@users.noreply.github.com>
2022-04-09 20:17:56 +0530

    Convert TrypClass from record to class

--- src/me/minefreak19/tryp/eval/TrypClass.java
+++ src/me/minefreak19/tryp/eval/TrypClass.java
@@ -2,8 +2,17 @@ package me.minefreak19.tryp.eval;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
-public record TrypClass(String name, Map<String, TrypProc> methods) implements TrypCallable {
+public final class TrypClass implements TrypCallable {
+	private final String name;
+	private final Map<String, TrypProc> methods;
+
+	public TrypClass(String name, Map<String, TrypProc> methods) {
+		this.name = name;
+		this.methods = methods;
+	}
+
 	public TrypProc findMethod(String name) {
 		return methods.get(name);
 	}
@@ -33,5 +42,22 @@ public record TrypClass(String name, Map<String, TrypProc> methods) implements T
 		}
 
 		return instance;
+	}
+
+	public String name() {
+		return name;
+	}
+
+	public Map<String, TrypProc> methods() {
+		return methods;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == this) return true;
+		if (obj == null || obj.getClass() != this.getClass()) return false;
+		var that = (TrypClass) obj;
+		return Objects.equals(this.name, that.name) &&
+				       Objects.equals(this.methods, that.methods);
 	}
 }
