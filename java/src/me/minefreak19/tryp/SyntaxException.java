b77227741b9468b65e192b503c4472984b14d0ba
minefreak19 <61090340+minefreak19@users.noreply.github.com>
2022-04-10 17:11:28 +0530

    Move java interpreter to java/ folder
    This will make development of VM and fleshing out of language via
    examples simpler as they each will get their own directory.

--- java/src/me/minefreak19/tryp/SyntaxException.java
+++ java/src/me/minefreak19/tryp/SyntaxException.java
@@ -1,11 +1,11 @@
 package me.minefreak19.tryp;
 
 import me.minefreak19.tryp.lex.FileLocation;
-import me.minefreak19.tryp.lex.token.Token;
 
 public class SyntaxException extends RuntimeException {
 
 	private final FileLocation loc;
+
 	public SyntaxException(FileLocation loc, String message) {
 		super(message);
 		this.loc = loc;
